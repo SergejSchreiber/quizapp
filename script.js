9c664c20cd4ad2c733a1cfc7e3adda3ffed05dc9
Sergej Schreiber <sn.schreiber@gmail.com>
2023-05-11 15:33:20 +0200

    final version

--- script.js
+++ script.js
@@ -25,21 +25,43 @@ let questions = [
   },
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;
+let audio_correct = new Audio("audio/correct.mp3");
+let audio_wrong = new Audio("audio/wrong.mp3");
 
 function init() {
   showQuestion();
+  document.getElementById("ofXquestion").innerHTML = questions.length;
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
-}
+  if (currentQuestion >= questions.length) {
+    showEndScreen();
+  } else {
+    document.getElementById("Xofquestion").innerHTML = currentQuestion + 1;
 
+    let percent = Math.round(((currentQuestion + 1) / questions.length) * 100);
+    console.log("Fortschritt", percent);
+    document.getElementById("progress-bar").innerHTML = `${percent} %`;
+    document.getElementById("progress-bar").style = `width: ${percent}%`;
+
+    let question = questions[currentQuestion];
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
+}
+function showEndScreen() {
+  document.getElementById("endscreen").style = "";
+  document.getElementById("questionbody").style = "display: none";
+  document.getElementById("score-arrows").style = "display: none";
+
+  document.getElementById("score-ofXquestion").innerHTML = questions.length;
+  document.getElementById("score-Xofquestion").innerHTML = rightQuestions;
+}
 function answer(selection) {
   let question = questions[currentQuestion];
   console.log("selected answer is ", selection);
@@ -51,9 +73,12 @@ function answer(selection) {
 
   if (selectedQuestionNumber == question["right_answer"]) {
     document.getElementById(selection).parentNode.classList.add("answerRight");
+    rightQuestions++;
+    audio_correct.play();
   } else {
     document.getElementById(selection).parentNode.classList.add("answerWrong");
     document.getElementById(IdOfRightAnswer).parentNode.classList.add("answerRight");
+    audio_wrong.play();
   }
 
   document.getElementById("arrowright").disabled = false;
@@ -75,4 +100,13 @@ function resetAnswerButtons() {
   document.getElementById("answer_3").parentNode.classList.remove("answerRight");
   document.getElementById("answer_4").parentNode.classList.remove("answerWrong");
   document.getElementById("answer_4").parentNode.classList.remove("answerRight");
+}
+
+function replay() {
+  rightQuestions = 0;
+  currentQuestion = 0;
+  document.getElementById("endscreen").style = "display: none";
+  document.getElementById("questionbody").style = "";
+  document.getElementById("score-arrows").style = "";
+  init();
 }
